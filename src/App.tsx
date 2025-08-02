3572ac42bfff9623899e1520cb8ff2b05c762890
rajtiller <raj.tiller@gmail.com>
2025-08-02 14:29:06 -0500

    added valid words and guesses

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import validGuessesData from "./data/words.json";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // Type assertion to tell TypeScript the structure
+  const validGuesses = validGuessesData as { guesses: string[], answers: string[] };
 
   return (
     <>
@@ -19,7 +23,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Some word: {validGuesses.answers[count % validGuesses.answers.length]}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/converter.py
@@ -0,0 +1,18 @@
+# #!/usr/bin/env python3
+# import json
+# answers = []
+# guesses = []
+# with open("/Users/rajtiller/Source/React-Projects/Projects/Unassigned Projects/wordle/src/data/answers.txt", "r") as file:
+#     for line in file:
+#         # Process each line here
+#         word = line.strip()
+#         if word:
+#             answers.append(word)
+
+# with open("/Users/rajtiller/Source/React-Projects/Projects/Unassigned Projects/wordle/src/data/guesses.txt", "r") as file:
+#     for line in file:
+#         word = line.strip()
+#         if word:
+#             guesses.append(word)
+# with open("/Users/rajtiller/Source/React-Projects/Projects/Unassigned Projects/wordle/src/data/words.json", "w") as file:
+#     json.dump({"answers": answers, "guesses":guesses}, file, indent=2)
